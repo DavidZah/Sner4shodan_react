d74359f92a1b16b6abe7a757024708323984fa9a
DavidZah <davidzah6@seznam.cz>
2019-07-24 14:40:47 +0200

    funcionality finish

--- src/Components/Host.js
+++ src/Components/Host.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import PropTypes from "prop-types";
-import { withRouter } from "react-router";
 import {getJsonData} from './../Api'
 import {ErrorNotFound} from './NotFound'
-
+import Services from './Service';
 
 function BackButton(props) {
         return (
@@ -17,12 +15,29 @@ function BackButton(props) {
 
 class Host extends React.Component {
 
-  
+    findId(json_data){
+      var id_name = this.props.match.params.id;
+      
+      for(var i in json_data.data){
+        if(json_data.data[i].host_name == id_name) {
+          this.state = { hostFound:true};
+          return(json_data.data[i]); 
+        }
+        
+      } 
+      this.state = { hostFound:false};
+    }
+
     render(){
+      const json_data = getJsonData();
+      var x = this.findId(json_data);
+      if(this.state.hostFound){
+        return(
+            <Services renderOneService={true} service={x} />
+        );
+      }
       return(
-          <div>
           <ErrorNotFound/>
-        </div>
       );
     }
 }
