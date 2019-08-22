a05fbf15d66e4de5ae4ba4a60320ce708984f207
DavidZah <davidzah6@seznam.cz>
2019-08-22 14:30:12 +0200

    Rdy_to_revision

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,14 +3,12 @@ import { Services } from './Service';
 import { getJsonData } from './../Api';
 
 export function Home() {
-
     const json_data = getJsonData();
     return (
-        
         <div>
             {json_data.data.map((service, serviceKey) => (
-                    <div className="card-body"  key={serviceKey}>
-                        <h2>{service.host_name}</h2>
+                <div className="card-body" key={serviceKey}>
+                    <h2>{service.host_name}</h2>
                     <div>
                         <Services renderOneService={true} service={service} />
                     </div>
