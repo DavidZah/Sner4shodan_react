c6966e16bd3b49a6b2510fec4e448754bf5d48f0
DavidZah <davidzah6@seznam.cz>
2019-09-10 18:53:13 +0200

    Add babel and npm finally fix

--- src/app.react.test.js
+++ src/app.react.test.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import appRouter from 'app.js'
-import renderer from 'react-test-renderer';
-import TestRunner from 'jest-runner';
+import React from 'react';  
+import { Hosts } from './Components/Hosts';
+import renderer from 'react-test-renderer';  
 
-test('Test crating by dave', () => {
-    const component = renderer.create(
-        <appRouter/>
-    );
-    let tree = component.toJSON();
-  
-    expect(tree).toNotThrowError();
-}); 
+it('renders correctly', () => {  
+const tree = renderer.create(<Hosts />).toJSON();  
+  expect(tree).toMatchSnapshot();  
+});
